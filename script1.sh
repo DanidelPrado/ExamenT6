07f91fea50fc1c663c3016e9b696fc83d25f3aec
DanidelPrado <snait7@gmail.com>
2020-05-04 19:01:06 +0200

    acabado el script

--- script1.sh
+++ script1.sh
@@ -1,8 +1,10 @@
 read -p "Introduce un valor mayor que 0: " x
 cont=0;
 if [ $x -lt 0 ]; then
-    while [ $x -lt 0 ]; do
-	read -p "El valor introducido es incorrecto. Introduce un valor mayor que 0: " x
-    done
+    echo "El valor es menor que 0."
+else
+while [ $cont -le $x ]; do
+    echo "$cont"
+    cont=`expr $cont + 5`
+done
 fi
-
